acb42b815687ddb0b561c9befec209e26c82468d
schwalski <schwalski@gmail.com>
2016-11-21 20:46:24 +0900

    make rctrl and tab

--- keyboards/ergodox/keymaps/schwalski/keymap.c
+++ keyboards/ergodox/keymaps/schwalski/keymap.c
@@ -46,22 +46,22 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Keymap 0: Basic layer
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
- * | `~     |   1! |   2@ |   3# |   4$ |   5% | ESC  |           |CmdAES|   6^ |   7& |   8* |   9( |   0) |   =+   |
+ * | `~     |   1! |   2@ |   3# |   4$ |   5% | ESC  |           |      |   6^ |   7& |   8* |   9( |   0) |   =+   |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * | Tab    |   Q  |   W  |   E  |   R  |   T  |  [{  |           |  ]}  |   Y  |   U  |   I  |   O  |   P  |   -_   |
+ * | Tab    |   Q  |   W  |   E  |   R  |   T  |  [{  |           |  }]  |   Y  |   U  |   I  |   O  |   P  |   -_   |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * | LCtrl  |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |  ;:  |   '"   |
- * |--------+------+------+------+------+------|  (   |           |  )   |------+------+------+------+------+--------|
+ * |--------+------+------+------+------+------|  Tab |           |  `~  |------+------+------+------+------+--------|
  * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |  ,<  |  .>  |  /?  |   \|   |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |      |      |      |      | LAlt |                                       | RAlt |  BS  |  DEL |      |      |
+ *   |Hyper |CmdAES|      |      | LAlt |                                       | RAlt |  BS  |  DEL |      |      |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,---------------.
  *                                        |Cmd sp|      |       |      | ctl F3 |
  *                                 ,------|------|------|       |------+--------+------.
  *                                 |      |      |      |       |      |        |      |
  *                                 | Space|LShift|------|       |------| RShift | Enter|
- *                                 |  Cmd |      | LCtrl|       | Hyper|        | ->L2 |
+ *                                 |  Cmd |      | LCtrl|       | RCtrl|        | ->L2 |
  *                                 `--------------------'       `----------------------'
  */
 // If it accepts an argument (i.e, is a function), it doesn't need KC_.
@@ -71,8 +71,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_GRV,     KC_1,       KC_2,       KC_3,       KC_4,       KC_5,       KC_ESC,
     KC_TAB,     KC_Q,       KC_W,       KC_E,       KC_R,       KC_T,       KC_LBRC,
     KC_LCTL,    KC_A,       KC_S,       KC_D,       KC_F,       KC_G,
-    KC_LSFT,    KC_Z,       KC_X,       KC_C,       KC_V,       KC_B,       S(KC_9),
-    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_LALT,
+    KC_LSFT,    KC_Z,       KC_X,       KC_C,       KC_V,       KC_B,       KC_TAB,
+    KC_HYPR,    RGUI(A(KC_ESC)),    KC_TRNS,    KC_TRNS,    KC_LALT,
 
     LGUI(KC_SPC),   KC_TRNS,
     KC_TRNS,
@@ -80,15 +80,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 
     //righthand
-    RGUI(A(KC_ESC)), KC_6,       KC_7,       KC_8,       KC_9,       KC_0,       KC_EQL,
+    KC_TRNS,    KC_6,       KC_7,       KC_8,       KC_9,       KC_0,       KC_EQL,
     KC_RBRC,    KC_Y,       KC_U,       KC_I,       KC_O,       KC_P,       KC_MINS,
                 KC_H,       KC_J,       KC_K,       KC_L,       KC_SCLN,    KC_QUOT,
-    S(KC_0),    KC_N,       KC_M,       KC_COMM,    KC_DOT,     KC_SLSH,    KC_BSLS,
+    KC_GRV,     KC_N,       KC_M,       KC_COMM,    KC_DOT,     KC_SLSH,    KC_BSLS,
                             KC_RALT,    KC_BSPC,    KC_DEL,     KC_TRNS,    KC_TRNS,
 
     KC_TRNS,  C(KC_F3),
     KC_TRNS,
-    KC_HYPR,  KC_RSFT,    LT(L_CUR,KC_ENT)
+    KC_RCTL,  KC_RSFT,    LT(L_CUR,KC_ENT)
 ),
 
 
