b87487e39c56f93385f22f4ef907a497b29f17d4
schwalski <schwalski@gmail.com>
2016-11-23 19:57:30 +0900

    replace mause to ten-key

--- keyboards/ergodox/keymaps/schwalski/keymap.c
+++ keyboards/ergodox/keymaps/schwalski/keymap.c
@@ -34,6 +34,7 @@ enum {
   L_WIN, // Windows
   L_QKDW, // QKDW
   L_CUR, // mouse and cursor
+  L_TEN, // tenkey
   L_CONF // config
 };
 
@@ -88,7 +89,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     KC_TRNS,  C(KC_F3),
     KC_TRNS,
-    KC_RCTL,  KC_RSFT,    LT(L_CUR,KC_ENT)
+    KC_RCTL,  KC_RSFT,    LT(L_TEN,KC_ENT)
 ),
 
 
@@ -211,7 +212,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TRNS,      KC_F1,          KC_F2,          KC_F3,          KC_F4,          KC_F5,            KC_F11,
     KC_TRNS,      KC_TRNS,        KC_TRNS,        KC_M_U,         KC_TRNS,        KC_TRNS,          KC_LPRN,
     KC_TRNS,      KC_TRNS,        KC_M_L,         KC_M_D,         KC_M_R,         KC_TRNS,
-    KC_TRNS,      SFT_T(KC_1),           KC_2,           KC_3,           KC_4,           KC_5,             SC(KC_TAB),
+    KC_TRNS,      SFT_T(KC_1),    KC_2,           KC_3,           KC_4,           KC_5,             SC(KC_TAB),
     KC_TRNS,      KC_TRNS,        KC_TRNS,        KC_M_B3,        KC_M_B2,
 
                   KC_TRNS,        KC_TRNS,
@@ -226,6 +227,52 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                   KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS,          KC_TRNS,
 
     KC_TRNS,      KC_TRNS,
+    KC_TRNS,
+    KC_TRNS,      KC_TRNS,   KC_TRNS
+),
+
+
+/* Keymap: Cursor, TenKey Layer
+ *
+ * ,--------------------------------------------------.           ,--------------------------------------------------.
+ * |        |  F1  |  F2  |  F3  |  F4  |  F5  |  F11 |           | F12  |  F6  |  F7  |  F8  |  F9  |  F10 | ->CNF  |
+ * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
+ * |        |      |  7   |  8   |  9   |  -   |  (   |           |  )   | Home | PgUp |  Up  | PgDn |      |        |
+ * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
+ * |        |      |  4   |  5   |  6   |  +   |------|           |------| End  | Left | Down | Right|   :  |        |
+ * |--------+------+------+------+------+------|SC Tab|           | C Tab|------+------+------+------+------+--------|
+ * |        |      |  1   |  2   |  3   |  =   |      |           |      |   6^ |   7& |   8* |   9( |   0) |        |
+ * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
+ *   |      |      |  0   |  0   |  .   |                                       |      |      |      |      |      |
+ *   `----------------------------------'                                       `----------------------------------'
+ *                                        ,-------------.       ,-------------.
+ *                                        |      |      |       |      |      |
+ *                                 ,------|------|------|       |------+------+------.
+ *                                 |      |      |      |       |      |      |      |
+ *                                 | Enter| BS   |------|       |------|      |      |
+ *                                 |      |      |      |       |      |      |      |
+ *                                 `--------------------'       `--------------------'
+ */
+[L_TEN] = KEYMAP(
+    // left hand
+    KC_TRNS,      KC_TRNS,        KC_ESC,         KC_TAB,         KC_BSPC,        KC_EQL,           KC_TRNS,
+    KC_TRNS,      KC_TRNS,        KC_7,           KC_8,           KC_9,           KC_MINS,          KC_LPRN,
+    KC_TRNS,      KC_TRNS,        KC_4,           KC_5,           KC_6,           S(KC_EQL),
+    KC_TRNS,      KC_TRNS,        KC_1,           KC_2,           KC_3,           KC_EQL,           SC(KC_TAB),
+    KC_TRNS,      KC_TRNS,        KC_0,           KC_0,           KC_DOT,
+
+                  KC_TRNS,        KC_TRNS,
+                                  KC_TRNS,
+    KC_ENT,       KC_BSPC,        KC_TRNS,
+
+    //righthand
+    KC_F12,       KC_F6,          KC_F7,          KC_F8,          KC_F9,          KC_F10,           MO(L_CONF),
+    KC_RPRN,      KC_HOME,        KC_PGUP,        KC_UP,          KC_PGDN,        KC_TRNS,          KC_TRNS,
+                  KC_END,         KC_LEFT,        KC_DOWN,        KC_RIGHT,       KC_COLN,          KC_TRNS,
+    C(KC_TAB),    KC_6,           KC_7,           KC_8,           KC_9,           SFT_T(KC_0),      KC_TRNS,
+                                  KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS,          KC_TRNS,
+
+    TG(L_TEN),    KC_TRNS,
     KC_TRNS,
     KC_TRNS,      KC_TRNS,   KC_TRNS
 ),
@@ -274,7 +321,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TRNS,    KC_TRNS,
     KC_TRNS,
     KC_TRNS,    KC_TRNS,    KC_TRNS
-)
+),
 };
 
 const uint16_t PROGMEM fn_actions[] = {
@@ -313,16 +360,10 @@ void matrix_scan_user(void) {
     ergodox_right_led_3_off();
     switch (layer) {
       // TODO: Make this relevant to the ErgoDox EZ.
-        case 0:
-        case 3:
+        case L_MAC:
+        case L_TEN:
             break;
-        case 1:
-            ergodox_right_led_1_on();
-            break;
-        case 2:
-            ergodox_right_led_2_on();
-            break;
-        case 4:
+        case L_CONF:
             ergodox_right_led_3_on();
             break;
         default:
