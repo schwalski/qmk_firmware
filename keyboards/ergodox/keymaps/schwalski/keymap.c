dc3f614eafb06c242ec2cee28ff32238256d0f75
schwalski <schwalski@gmail.com>
2016-11-27 19:17:45 +0900

    enable f1

--- keyboards/ergodox/keymaps/schwalski/keymap.c
+++ keyboards/ergodox/keymaps/schwalski/keymap.c
@@ -255,7 +255,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [L_NUM] = KEYMAP(
     // left hand
-    KC_TRNS,      KC_TRNS,        KC_F2,          KC_F3,          KC_F4,          KC_F5,            KC_F11,
+    KC_TRNS,      KC_F1,          KC_F2,          KC_F3,          KC_F4,          KC_F5,            KC_F11,
     KC_TRNS,      KC_TRNS,        KC_7,           KC_8,           KC_9,           KC_EQL,           KC_LPRN,
     KC_TRNS,      KC_TRNS,        KC_4,           KC_5,           KC_6,           KC_MINS,
     KC_TRNS,      KC_TRNS,        KC_1,           KC_2,           KC_3,           S(KC_EQL),       SC(KC_TAB),
